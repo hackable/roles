cb78761db65df1fa1181a8d002b28f5be1733074
cooldeark <cooldeark@gmail.com>
2024-05-30 19:50:25 +0800

    minor fix

--- database/migrations/create_user_table.php
+++ database/migrations/create_user_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // 使用 Laravel 默认的 bigIncrements 类型
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
