87232038d7d47039ff6213114fdba70b96c766cf
cooldeark <cooldeark@gmail.com>
2024-05-30 19:43:05 +0800

    fix test case & support laravel11

--- database/migrations/create_role_user_table.php
+++ database/migrations/create_role_user_table.php
@@ -14,11 +14,9 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->id();
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,8 +29,8 @@ class CreateRoleUserTable extends Migration
     public function down()
     {
         Schema::table('role_user', function (Blueprint $table) {
-            $table->dropForeign('role_user_role_id_foreign');
-            $table->dropForeign('role_user_user_id_foreign');
+            $table->dropForeign(['role_id']);
+            $table->dropForeign(['user_id']);
         });
         Schema::dropIfExists('role_user');
     }
